08ceb536e014aff1d66b3c393e0861fa036e465c
Sharanya Sudhakar <sharanu@uw.edu>
2019-06-27 21:57:15 -0700

    flipper added to main menu... changes made to the mainmenu- light and animation sped up.. whale position also moved...- sound files added back

---
+++ Assets/CameraTextTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTextTimer : MonoBehaviour
+{
+    [SerializeField] float disappearTime;
+    [SerializeField] GameObject text;
+    float starttime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        starttime = Time.deltaTime;
+        text.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        disappearTime -= Time.deltaTime;
+        if(disappearTime<0)
+            text.SetActive(false);
+
+    }
+}
--- Assets/Scripts/RockEffectsManager.cs
+++ Assets/Scripts/RockEffectsManager.cs
@@ -7,6 +7,7 @@ public class RockEffectsManager : MonoBehaviour {
     public MusicRock rock;
     public Light light;
     public ParticleSystem ps;
+    public AudioSource startSound;
     public ParticleSystem attentionParticles;
     public ParticleSystem fishSystem;
     public GameObject symbolMat;
@@ -20,8 +21,8 @@ public class RockEffectsManager : MonoBehaviour {
 
     [Header("ColorFadeSpeeds")]
     public float colorDeactivteFadeSpeed = 0.25f;
-    public float colorActivateFadeSpeed = 0.25f;
-    public float colorSelectedFadeSpeed = 0.5f;
+    public float colorActivateFadeSpeed = 0.75f;
+    public float colorSelectedFadeSpeed = 0.57f;
     public float colorDeselectedFadeSpeed = 1.5f;
 
     [Header("Activation Angle Settings")]
@@ -49,13 +50,15 @@ public class RockEffectsManager : MonoBehaviour {
     }
 
     void Select() {
-        light.intensity *= 2;
+        // light.intensity *= 8;
+        light.intensity = 23;
         Material mat = symbolMat.GetComponent<Renderer>().material;
         fadeColor(selectedColor, colorSelectedFadeSpeed, true, false);
     }
 
     void Deselect() {
-        light.intensity /= 2;
+        // light.intensity /= 8;
+        light.intensity = 0;
         Material mat = symbolMat.GetComponent<Renderer>().material;
         fadeColor(baseColor, colorDeselectedFadeSpeed, true, false);
     }
@@ -125,7 +128,10 @@ public class RockEffectsManager : MonoBehaviour {
         }
         mat.SetColor("_EmissionColor", color);
 
-        if(bubble)
+        if (bubble)
+        {
             ps.Play();
+            startSound.Play();
+        }
     }
 }
