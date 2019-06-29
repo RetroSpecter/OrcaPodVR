82f24e284367126295979df1b37be86fbec385e9
Sharanya Sudhakar <sharanu@uw.edu>
2019-06-29 04:42:41 -0700

    talequa fins added.. needs testing and boat script disabled

---
+++ Assets/FinsScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinsScript : MonoBehaviour
+{
+    [SerializeField] GameObject left, right;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        left.transform.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+        right.transform.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+    }
+}
